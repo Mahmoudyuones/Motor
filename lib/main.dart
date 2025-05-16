b99616e746aa276a1d718c1b2e4b341234933854
Mahmoud Younes <mywns211@gmail.com>
2025-05-17 02:24:23 +0300

    add   await ScreenUtil.ensureScreenSize();

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:motor/features/register_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await ScreenUtil.ensureScreenSize();
   runApp(const MyApp());
 }
 
